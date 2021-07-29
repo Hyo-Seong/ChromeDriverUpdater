d8156dbdf8958167b9f3a767c65fa5670c6af5eb
Hyo-Seong <gytjd090792@gmail.com>
2021-07-29 21:10:00 +0900

    Add UpdateFailException and ExitCode enum

---
+++ Src/ChromeDriverUpdater/UpdateFailException.cs
@@ -0,0 +1,30 @@
+﻿using ChromeDriverUpdater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeDriverUpdater
+{
+    public class UpdateFailException : Exception
+    {
+        public ExitCode ExitCode { get; set; }
+
+        public UpdateFailException()
+        {
+        }
+
+        public UpdateFailException(string message, ExitCode exitCode)
+            : base(message)
+        {
+            ExitCode = exitCode;
+        }
+
+        public UpdateFailException(string message, ExitCode exitCode, Exception inner)
+            : base(message, inner)
+        {
+            ExitCode = exitCode;
+        }
+    }
+}
