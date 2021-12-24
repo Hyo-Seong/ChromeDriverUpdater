770fb9290bbeb582b7b67402a044a6f95ed7a96d
Hyo-Seong <gytjd090792@gmail.com>
2021-12-24 21:20:21 +0900

    Remove unused using

--- Src/ChromeDriverUpdater/Exception/UpdateFailException.cs
+++ Src/ChromeDriverUpdater/Exception/UpdateFailException.cs
@@ -1,9 +1,5 @@
 ﻿using ChromeDriverUpdater.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChromeDriverUpdater
 {
