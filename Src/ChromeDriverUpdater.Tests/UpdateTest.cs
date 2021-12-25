63501294d169bdd76812d4b6be59842ab042cd60
Henry (HyoSeong) Lee <gytjd090792@gmail.com>
2021-12-25 18:58:09 +0900

    Add WrongChromeDriverPathTest

--- Src/ChromeDriverUpdater.Tests/UpdateTest.cs
+++ Src/ChromeDriverUpdater.Tests/UpdateTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using NUnit.Framework;
 
@@ -14,9 +13,33 @@ namespace ChromeDriverUpdater.Tests
             File.Copy("chromedriver_old.exe", "chromedriver.exe");
 
             Updater updater = new Updater();
-            updater.Update("chromedriver.exe");
+            try
+            {
+                updater.Update("chromedriver.exe");
+            } 
+            catch(UpdateFailException exc)
+            {
+                // In Github action, Chrome is not downloaded.
+                // Or cannot access to registry.
+                Assert.IsTrue(exc.ErrorCode == Models.ErrorCode.ChromeNotInstalled);
+            }
 
             Assert.True(true);
         }
+
+        [Test]
+        public void WrongChromeDriverPathTest()
+        {
+            try
+            {
+                Updater updater = new Updater();
+                updater.Update("wrong.exe");
+            }
+            catch(UpdateFailException exc)
+            {
+                Assert.IsTrue(exc.ErrorCode == Models.ErrorCode.ChromeDriverNotFound);
+            }
+        }
+
     }
 }
